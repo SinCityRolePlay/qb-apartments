2824885775053e206704e90a5f761116abea2b05
SinisterMind81 <bradkenny81@yahoo.com>
2022-05-24 00:37:39 -0500

    door change for integrity apartments

--- config.lua
+++ config.lua
@@ -40,12 +40,12 @@ Apartments.Locations = {
         name = "apartment3",
         label = "Integrity Way",
         coords = {
-            enter = vector4(269.73, -640.75, 42.02, 249.07),
+            enter = vector4(281.2, -639.92, 4.65, 71.16),
         },
         polyzoneBoxData = {
             heading = 250,
-            minZ = 40,
-            maxZ = 43.5,
+            minZ = 2,
+            maxZ = 6.5,
             debug = false,
             length = 1,
             width = 1,
